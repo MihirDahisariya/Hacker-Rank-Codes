85c7483c5fad482edd9adfab316cfe886df95333
Mihir Dahisariya <61309840+MihirDahisariya@users.noreply.github.com>
2022-09-28 17:16:00 +0530

    Add files via upload

---
+++ 1 Month Interview Preparation/Week 1/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+
+    public static void plusMinus(List<Integer> arr) 
+    {
+        int plus = 0, minus = 0, zero = 0;
+        int n = arr.size();
+
+        for (Integer value:arr)
+        {
+            if ( value > 0 )
+            {
+                plus++;
+            }
+            else if ( value < 0)
+            {
+                minus++;
+            }
+            else
+            {
+                zero++;
+            }    
+        }
+        System.out.printf("%.6f \n",(float)plus/n);
+        System.out.printf("%.6f \n",(float)minus/n);
+        System.out.printf("%.6f \n",(float)zero/n);
+    }
+
+}
+
+public class Solution 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
